9c744bf0ff70f18f4a34a9a9cf507923203b5022
xiapengfei <2358114512@qq.com>
2025-07-14 22:53:11 +0800

    add erc20

---
+++ fundme-token/FundTokenSimple02.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FundTokenERC20 is ERC20{
+    constructor() ERC20 ("FundTokenERC20", "FT") {}
+}
